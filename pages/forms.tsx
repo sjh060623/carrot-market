0336d655f0c37ef5580157b79013bf150503879f
손정훈 <sjh060623@gmail.com>
2022-02-15 21:27:14 +0900

    carrot-market 7.4

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,24 +1,39 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  username: string;
+  email: string;
+  password: string;
+}
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
-  const onValue = () => {
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onValid = (data: LoginForm) => {
     console.log("success");
   };
+  const onInvalid = (error: FieldErrors) => {
+    console.log(error);
+  };
   return (
-    <form onSubmit={handleSubmit(onValue)}>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register("username", { required: true })}
+        {...register("username", {
+          required: "Username is required",
+          minLength: {
+            message: "The username should be longer than 2 chars.",
+            value: 2,
+          },
+        })}
         type="text"
         placeholder="Username"
       />
       <input
-        {...register("email", { required: true })}
+        {...register("email", { required: "Email is required" })}
         type="Email"
         placeholder="Email"
       />
       <input
-        {...register("password", { required: true })}
+        {...register("password", { required: "Password is required" })}
         type="password"
         placeholder="Password"
       />
